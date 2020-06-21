ec9768cf4146f25c7916cd639f12473be6a2097d
Sohail <thegrabsinfo@gmail.com>
2020-06-21 13:43:42 +0500

    Table generating program

---
+++ table.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(final String[] args) {
+        // let's now print the table
+        // uska lia huma user sa ek input krwana hai
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int a = sc.nextInt();
+        System.out.print(a);
+       
+
+        sc.close();
+    }
+}
